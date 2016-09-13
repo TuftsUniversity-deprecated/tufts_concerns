9918aa48c8783b9cf1ccd055fec08a29df65ee4c
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-09-13 09:10:49 -0400

    Sending mike my indexer

--- app/indexers/voting_record_indexer.rb
+++ app/indexers/voting_record_indexer.rb
@@ -2,7 +2,15 @@ class VotingRecordIndexer < CurationConcerns::WorkIndexer
   def generate_solr_document
     super.tap do |solr_doc|
 
-      solr_doc['voting_record_xml_tesi'] = "datastream content"
+      l = Logger.new(STDOUT)
+      l.info(object.file_sets[0].files.class)
+      l.info(object.file_sets[0].files.empty?)
+      l.info(object.file_sets[0].files)
+      byebug
+
+      #:f3_pid
+
+      solr_doc['voting_record_xml_tesi'] = "datastream blah content"
 
     end
   end
