da8975b12197080c135bdcc175ee92436d74e6da
Mike Korcynski <mkorcy@gmail.com>
2017-01-17 09:39:46 -0500

    namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-  
+#Rails.application.routes.draw do
+TuftsModelsNg::Engine.routes.draw do
   mount UserImpersonate::Engine => "/impersonate", as: "impersonate_engine"
   mount Blacklight::Engine => '/'
   
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
     concerns :searchable
   end
 
-  devise_for :users
+  # devise_for :users
   mount CurationConcerns::Engine, at: '/'
-  resources :welcome, only: 'index'
-  root 'welcome#index'
+  # resources :welcome, only: 'index'
+  root 'catalog#index'
   curation_concerns_collections
   curation_concerns_basic_routes
   curation_concerns_embargo_management
