49b4d3c5449297c0d5990f76eac07f96350d1cff
Mike Korcynski <mkorcy@gmail.com>
2017-01-24 15:30:46 -0500

    accidental commit removed

--- lib/generators/tufts_models_ng/install_generator.rb
+++
@@ -1,9 +0,0 @@
-module TuftsModelsNg
-  class Install < Rails::Generators::Base
-    source_root File.expand_path('../templates', __FILE__)
-    def fuck
-
-    eay_status('warning', '[Hyrax] GENERATING BLACKLIGHT', :yellow)
-d
-  end
-end
