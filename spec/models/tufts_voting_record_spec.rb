0b8446819c13e996796ae08fca764d0794c8bc56
Mike Korcynski <mkorcy@gmail.com>
2017-01-24 11:16:07 -0500

    migrated specs from tufts_models

--- spec/models/tufts_voting_record_spec.rb
+++ spec/models/tufts_voting_record_spec.rb
@@ -1,9 +1,33 @@
-# Generated via
-#  `rails generate curation_concerns:work TuftsVotingRecord`
-require 'rails_helper'
+require 'spec_helper'
 
 describe TuftsVotingRecord do
-  it "has tests" do
-    skip "Add your tests here"
+  it 'has methods to support a draft version of the object' do
+    expect(described_class).to respond_to(:build_draft_version)
   end
+
+  describe "#default_datastream" do
+    subject { described_class.default_datastream }
+    it { is_expected.to eq 'RECORD-XML' }
+  end
+
+  describe "to_class_uri" do
+    subject { described_class.to_class_uri }
+    it { is_expected.to eq 'info:fedora/cm:VotingRecord' }
+  end
+
+
+  describe '#to_solr' do
+    let(:record) { TuftsVotingRecord.new(pid: 'tufts:ny.someoffice.1', title: 'some title') }
+
+    before do
+      record.add_relationship(:has_model, 'info:fedora/cm:VotingRecord')
+    end
+
+    subject { record.to_solr }
+
+    it 'sets object type as Dataset' do
+      expect(subject['object_type_sim']).to eq ['Datasets']
+    end
+  end
+
 end
