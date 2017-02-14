73f5303fcd950f87c5c0d13f49bce0cdf90889e6
Mike Korcynski <mkorcy@gmail.com>
2017-02-14 09:55:22 -0500

    adds additional models

--- config/initializers/curation_concerns.rb
+++ config/initializers/curation_concerns.rb
@@ -1,6 +1,20 @@
 require 'curation_concerns'
 
 CurationConcerns.configure do |config|
+  # Injected via `rails g curation_concerns:work TuftsVideo`
+  config.register_curation_concern :tufts_video
+  # Injected via `rails g curation_concerns:work TuftsTei`
+  config.register_curation_concern :tufts_tei
+  # Injected via `rails g curation_concerns:work TuftsRcr`
+  config.register_curation_concern :tufts_rcr
+  # Injected via `rails g curation_concerns:work TuftsPdf`
+  config.register_curation_concern :tufts_pdf
+  # Injected via `rails g curation_concerns:work TuftsImage`
+  config.register_curation_concern :tufts_image
+  # Injected via `rails g curation_concerns:work TuftsEad`
+  config.register_curation_concern :tufts_ead
+  # Injected via `rails g curation_concerns:work TuftsAudio`
+  config.register_curation_concern :tufts_audio
   # Injected via `rails g curation_concerns:work TuftsGenericObject`
   config.register_curation_concern :tufts_generic_object
   # Injected via `rails g curation_concerns:work TuftsVotingRecord`
