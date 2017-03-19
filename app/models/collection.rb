a4cd5dc4388e9d85f32dbfc305002c3ae023e01e
Mike Korcynski <mkorcy@gmail.com>
2017-03-19 14:33:52 -0400

    include custom metadata

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,5 @@ class Collection < ActiveFedora::Base
   include ::CurationConcerns::CollectionBehavior
   # You can replace these metadata if they're not suitable
   include CurationConcerns::BasicMetadata
+  include CollectionMetadata
 end
