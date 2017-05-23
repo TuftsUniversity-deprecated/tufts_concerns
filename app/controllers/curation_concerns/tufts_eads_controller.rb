dc81fbd7359a66744c50406783458d9fa627ffec
bgoodmon <brian.goodmon@tufts.edu>
2017-05-23 15:01:49 -0400

    Moved load_fedora_document() to tdl_f4.

--- app/controllers/curation_concerns/tufts_eads_controller.rb
+++ app/controllers/curation_concerns/tufts_eads_controller.rb
@@ -7,16 +7,5 @@ module CurationConcerns
     include CurationConcerns::CurationConcernController
     include WithLimitedFileSets
     self.curation_concern_type = TuftsEad
-
-    before_action :load_fedora_document
-
-    def load_fedora_document
-      @document_fedora = ActiveFedora::Base.find(params[:id])
-
-      return unless @document_fedora.class.instance_of?(TuftsEad.class)
-
-      @document_ead = Datastreams::Ead.from_xml(@document_fedora.file_sets.first.original_file.content)
-      @document_ead.ng_xml.remove_namespaces! unless @document_ead.nil?
-    end
   end
 end
