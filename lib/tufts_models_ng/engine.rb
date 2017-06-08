d3399f4e4582443c8af75ab1dbeb83638dbc5f1b
Tom Johnson <tom@dp.la>
2017-06-08 07:57:13 -0700

    [HYRAX] don't rake

--- lib/tufts_models_ng/engine.rb
+++ lib/tufts_models_ng/engine.rb
@@ -18,7 +18,6 @@ module TuftsModelsNg
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
-          rake 'hyrax:install:migrations'
         end
       end
     end
