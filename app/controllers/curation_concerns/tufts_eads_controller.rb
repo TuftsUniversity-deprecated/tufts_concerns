625a12500e3317cdaf798a6556ab631d3fc520b8
bgoodmon <brian.goodmon@tufts.edu>
2017-06-21 11:53:38 -0400

    Moved app/helpers/eads_helper.rb and app/models/datastreams/ead.rb from tufts_concerns into tdl_f4.

--- app/controllers/curation_concerns/tufts_eads_controller.rb
+++ app/controllers/curation_concerns/tufts_eads_controller.rb
@@ -3,7 +3,6 @@
 
 module CurationConcerns
   class TuftsEadsController < ApplicationController
-    helper :eads
     include CurationConcerns::CurationConcernController
     include WithLimitedFileSets
     self.curation_concern_type = TuftsEad
